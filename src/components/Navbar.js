e7fa4f02e64f37c3391fc9fe1f50ce69a1a1dcc7
Rafael Sabatini <rafa.sabatini@yahoo.com.br>
2023-05-29 00:38:20 -0300

    criando navbar e footer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,23 @@ import React from 'react'
 
 import { NavLink } from 'react-router-dom'
 
+import styles from './Navbar.module.css'
+
 const Navbar = () => {
   return (
-    <div>
-      <NavLink to={'/'}>Home</NavLink>
-      <NavLink to={'/about'}>Sobre</NavLink>
-    </div>
+    <nav>
+      <NavLink to={'/'}>
+        Mini <span>Blog</span>
+      </NavLink>
+      <ul>
+        <li>
+          <NavLink to={'/'}>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to={'/about'}>Sobre</NavLink>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
